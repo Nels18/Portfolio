cc0f66498abb15185a989755a8941f580cd23798
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-02-09 22:48:45 +0100

    feature: update interative behavior

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,25 @@
 "use strict";
+/* Variables */
 const navbar = document.getElementById("navbar");
 const burger = document.getElementById("burger");
 const navbarLinks = document.querySelectorAll(".nav__link");
+const wrapper = document.querySelector(".slider__wrapper");
+const slideImg = document.querySelector(".slider__wrapper img");
+const controlPrev = document.getElementById("crl__prev");
+const controlNext = document.getElementById("crl__next");
+const sliderItems = document.getElementsByClassName("slider__item");
+const movePer = wrapper.scrollWidth / sliderItems.length;
+const maxMove = wrapper.style.width;
+let pressed = false;
+let startX = 0;
+let currentItem = 0;
+/* Fonctions */
 const toggleMenu = () => {
     navbar === null || navbar === void 0 ? void 0 : navbar.classList.toggle("active");
 };
 const hideMenu = () => {
     navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("active");
 };
-navbarLinks.forEach((link) => {
-    link.onclick = () => {
-        hideMenu();
-    };
-});
-burger.onclick = () => {
-    toggleMenu();
-};
-/******************/
-const wrapper = document.querySelector(".slider__wrapper");
-const slideImg = document.querySelector(".slider__wrapper img");
-let pressed = false;
-let startX = 0;
 const moveSliderItem = (event) => {
     if (!pressed)
         return;
@@ -35,25 +34,6 @@ const dragSliderItem = (event) => {
     startX = event.clientX;
     wrapper.style.cursor = "grabbing";
 };
-wrapper.onmousedown = (event) => {
-    dragSliderItem(event);
-};
-wrapper.onmouseup = () => {
-    dropSliderItem();
-};
-wrapper.onmousemove = (event) => {
-    moveSliderItem(event);
-};
-wrapper.onmouseleave = () => {
-    pressed = false;
-};
-/*********************/
-const controlPrev = document.getElementById("crl__prev");
-const controlNext = document.getElementById("crl__next");
-const sliderItems = document.getElementsByClassName("slider__item");
-const movePer = wrapper.scrollWidth / sliderItems.length;
-const maxMove = wrapper.style.width;
-let currentItem = 0;
 const enableControl = (control) => {
     control.classList.remove("disabled");
 };
@@ -74,12 +54,39 @@ const left_mover = () => {
     wrapper.scrollLeft -= movePer;
     currentItem--;
 };
+/* Code */
+// Menu
+navbarLinks.forEach((link) => {
+    link.onclick = () => {
+        hideMenu();
+    };
+});
+burger.onclick = () => {
+    toggleMenu();
+};
+// Slider
+wrapper.onmousedown = (event) => {
+    dragSliderItem(event);
+};
+wrapper.onmouseup = () => {
+    dropSliderItem();
+};
+wrapper.onmousemove = (event) => {
+    moveSliderItem(event);
+};
+wrapper.onmouseleave = () => {
+    pressed = false;
+};
+// Control
 controlNext.onclick = () => {
     if (controlNext === null || controlNext === void 0 ? void 0 : controlNext.classList.contains("disabled"))
         return;
     right_mover();
     if (currentItem === sliderItems.length) {
         disableControl(controlNext);
+    }
+    if (currentItem !== 0) {
+        enableControl(controlPrev);
     }
 };
 controlPrev.onclick = () => {
@@ -88,6 +95,9 @@ controlPrev.onclick = () => {
     left_mover();
     if (currentItem === 0) {
         disableControl(controlPrev);
+    }
+    if (currentItem !== sliderItems.length) {
+        enableControl(controlNext);
     }
 };
 document.onload = () => {
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,24 @@
+/* Variables */
+
 const navbar = document.getElementById("navbar");
 const burger = document.getElementById("burger");
 const navbarLinks = document.querySelectorAll<HTMLElement>(".nav__link");
+
+const wrapper = document.querySelector<HTMLElement>(".slider__wrapper");
+const slideImg = document.querySelector<HTMLElement>(".slider__wrapper img");
+
+const controlPrev = document.getElementById("crl__prev");
+const controlNext = document.getElementById("crl__next");
+const sliderItems = document.getElementsByClassName("slider__item");
+const movePer = wrapper!.scrollWidth / sliderItems.length;
+const maxMove = wrapper!.style.width;
+
+let pressed = false;
+let startX = 0;
+
+let currentItem = 0;
+
+/* Fonctions */
 
 const toggleMenu = () => {
   navbar?.classList.toggle("active");
@@ -9,24 +27,6 @@ const toggleMenu = () => {
 const hideMenu = () => {
   navbar?.classList.remove("active");
 };
-
-navbarLinks.forEach((link) => {
-  link!.onclick = () => {
-    hideMenu();
-  };
-});
-
-burger!.onclick = () => {
-  toggleMenu();
-};
-
-/******************/
-
-const wrapper = document.querySelector<HTMLElement>(".slider__wrapper");
-const slideImg = document.querySelector<HTMLElement>(".slider__wrapper img");
-
-let pressed = false;
-let startX = 0;
 
 const moveSliderItem = (event: MouseEvent) => {
   if (!pressed) return;
@@ -43,32 +43,6 @@ const dragSliderItem = (event: MouseEvent) => {
   startX = event.clientX;
   wrapper!.style.cursor = "grabbing";
 };
-
-wrapper!.onmousedown = (event: MouseEvent) => {
-  dragSliderItem(event);
-};
-
-wrapper!.onmouseup = () => {
-  dropSliderItem();
-};
-
-wrapper!.onmousemove = (event: MouseEvent) => {
-  moveSliderItem(event);
-};
-
-wrapper!.onmouseleave = () => {
-  pressed = false;
-};
-
-/*********************/
-
-const controlPrev = document.getElementById("crl__prev");
-const controlNext = document.getElementById("crl__next");
-const sliderItems = document.getElementsByClassName("slider__item");
-const movePer = wrapper!.scrollWidth / sliderItems.length;
-const maxMove = wrapper!.style.width;
-
-let currentItem = 0;
 
 const enableControl = (control: HTMLElement) => {
   control.classList.remove("disabled");
@@ -96,6 +70,40 @@ const left_mover = () => {
   currentItem--;
 };
 
+/* Code */
+
+// Menu
+
+navbarLinks.forEach((link) => {
+  link!.onclick = () => {
+    hideMenu();
+  };
+});
+
+burger!.onclick = () => {
+  toggleMenu();
+};
+
+// Slider
+
+wrapper!.onmousedown = (event: MouseEvent) => {
+  dragSliderItem(event);
+};
+
+wrapper!.onmouseup = () => {
+  dropSliderItem();
+};
+
+wrapper!.onmousemove = (event: MouseEvent) => {
+  moveSliderItem(event);
+};
+
+wrapper!.onmouseleave = () => {
+  pressed = false;
+};
+
+// Control
+
 controlNext!.onclick = () => {
   if (controlNext?.classList.contains("disabled")) return;
 
@@ -103,6 +111,10 @@ controlNext!.onclick = () => {
 
   if (currentItem === sliderItems.length) {
     disableControl(controlNext!);
+  }
+
+  if (currentItem !== 0) {
+    enableControl(controlPrev!);
   }
 };
 
@@ -114,13 +126,18 @@ controlPrev!.onclick = () => {
   if (currentItem === 0) {
     disableControl(controlPrev!);
   }
+
+  if (currentItem !== sliderItems.length) {
+    enableControl(controlNext!);
+  }
 };
 
 document.onload = () => {
   if (currentItem === sliderItems.length) {
     disableControl(controlNext!);
   }
+
   if (currentItem === 0) {
     disableControl(controlPrev!);
   }
-}
+};
